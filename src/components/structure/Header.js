1a423433d9550787630a0b32f2e83f4cd84b6d99
Emmanuel Sam <emnlsam873@gmail.com>
2024-05-11 10:07:34 +0530

    Logo added

---
+++ src/components/structure/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RenderHeader = () => {
+  return (
+    <div className='header'>
+      <a href='/'>
+        <div className='logo'></div>
+      </a>
+    </div>
+  )
+}
+
+export default RenderHeader
