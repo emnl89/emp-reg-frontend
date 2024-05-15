32b50780175286f4e4092cb158e0f1d067685256
antoalex07 <antoalex710@gmail.com>
2024-05-15 15:01:09 +0530

    about and service path added

--- src/components/structure/Navigation.js
+++ src/components/structure/Navigation.js
@@ -8,17 +8,21 @@ import Attendance from '../pages/Attendance'
 import EmployeeDetails from '../pages/EmployeeDetails'
 import Registry from '../pages/Registry'
 import UpdateAttendance from '../pages/UpdateAttendance'
+import About from '../pages/About'
+import Service from '../pages/Service'
 
 const nav = [
-    {path: "/",                 name: "Home",             element: <Home/>,             isMenu: true,  isPrivate: false},
-    {path: "/login",            name: "Log In",           element: <Login/>,            isMenu: false, isPrivate: false},
-    {path: "/signUp",           name: "Sign Up",          element: <SignUp/>,           isMenu: false, isPrivate: false},
-    {path: "/account",          name: "Account",          element: <Account/>,          isMenu: true,  isPrivate: true},
-    {path: "/calendar",         name: "Calendar",         element: <Calendar/>,         isMenu: false, isPrivate: true},
-    {path: "/calendar/day",     name: "Attendance",       element: <Attendance/>,       isMenu: false, isPrivate: false},
-    {path: "/employeeDetails",  name: "Employee Details", element: <EmployeeDetails/>,  isMenu: true,  isPrivate: true},
-    {path: "/registry",         name: "Registry",         element: <Registry/>,         isMenu: true,  isPrivate: true},
-    {path: "/updateAttendance",         name: "Update Attendance",         element: <UpdateAttendance/>,         isMenu: false,  isPrivate: true},
+    {path: "/",                  name: "Home",              element: <Home/>,             isMenu: true,  isPrivate: false},
+    {path: "/about",             name: "About",             element: <About/>,            isMenu: true,  isPrivate: false},
+    {path: "/services",          name: "Services",          element: <Service/>,          isMenu: true,  isPrivate: false},
+    {path: "/login",             name: "Log In",            element: <Login/>,            isMenu: false, isPrivate: false},
+    {path: "/signUp",            name: "Sign Up",           element: <SignUp/>,           isMenu: false, isPrivate: false},
+    {path: "/account",           name: "Account",           element: <Account/>,          isMenu: true,  isPrivate: true},
+    {path: "/calendar",          name: "Calendar",          element: <Calendar/>,         isMenu: false, isPrivate: true},
+    {path: "/calendar/day",      name: "Attendance",        element: <Attendance/>,       isMenu: false, isPrivate: false},
+    {path: "/employeeDetails",   name: "Employee Details",  element: <EmployeeDetails/>,  isMenu: true,  isPrivate: true},
+    {path: "/registry",          name: "Registry",          element: <Registry/>,         isMenu: true,  isPrivate: true},
+    {path: "/updateAttendance",  name: "Update Attendance", element: <UpdateAttendance/>, isMenu: false,  isPrivate: true},
 ]
 
 export default nav
