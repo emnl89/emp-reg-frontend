8605c49625df03bf1bab478a43fd83209207b22d
antoalex07 <antoalex710@gmail.com>
2024-05-15 15:00:54 +0530

    page before and after user login set

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,34 +1,39 @@
 import { Button } from '@mui/material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AuthData } from '../../auth/Authwrapper';
 
 const Home = () => {
   
   const navigate = useNavigate();
+  const { user } = AuthData();
+
   const handleButtonClick = () => {
     navigate("/signUp");
   }
 
   return (
     <div className='home'>
+      <div className='primary-text'>
+        {user.isAuthenticated ? (
+          <h1>Welcome to workforce management with attendify</h1>
+        ) : (
+          <h1>Streamline your workforce management with attendify</h1>
+        )}
+        <div className='section-divider'/>
+        <p>Effortlessly manage employee tasks, attendance, and wages with our intuitive platform</p>
+        {!user.isAuthenticated ? (
+            <Button sx={{ fontWeight: 900, 
+                          width: "500px", 
+                          fontFamily: "Lato, sans seriff"}}
+                    variant='contained'>
+          Sign Up</Button>    
+        ):(
+          <>
+          </>
+        )}
+      </div>
       <div className='banner-image'></div>
-      <h1 className='primary-heading'>
-        Want to track attendance and manage your expenses?
-      </h1>
-      <hr className='section-divider'/>
-      <p className='primary-text'>
-        Looking to simplify attendance tracking, streamline expense management,
-        and handle bills effortlessly? Explore our tools designed to enhance
-        productivity and streamline your administrative tasks.  
-      </p>
-      <Button 
-              style={{ fontSize: '40px',
-                       width: '20vw',
-                       fontFamily: "Reem Kufi, sans seriff"
-              }}
-              onClick={handleButtonClick}
-      >
-        Register Now</Button>
     </div>
   )
 }
