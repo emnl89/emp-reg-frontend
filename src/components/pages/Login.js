56ac05ec6822b5cc260d5dfcf314a6ea385de5ea
antoalex07 <antoalex710@gmail.com>
2024-05-17 11:20:54 +0530

    input columns added

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,10 @@
 import React, { useReducer, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthData } from '../../auth/Authwrapper';
+import { KeyboardBackspaceRounded } from '@mui/icons-material';
+import TodayRoundedIcon from '@mui/icons-material/TodayRounded';
+import { InputBase, Paper } from '@mui/material';
+
 
 const Login = () => {
 
@@ -21,7 +25,50 @@ const Login = () => {
     }
 
   return (
-    <div className="page">
+    <div className="login">
+      <div className='form-container'>
+        <div className='image-container'>
+          <KeyboardBackspaceRounded 
+                            htmlColor='#818283'
+                            style={{ position: 'absolute',
+                                    top: '30px',
+                                    left: '30px',
+                                    fontSize: '40px',   
+                                }}/>
+        </div>
+        <div className='login-form'>
+          <h1 className='heading'>Login</h1>
+          <Paper
+            component="form"
+            sx={{ marginTop: '6%',
+                  marginLeft: '12px',
+                  p: '10px 4px',
+                  display: 'flex',
+                  width: 300,
+                  borderRadius: '0px',
+                  alignItems: 'center',
+                  backgroundColor: '#c5e6f5' }}>
+            <TodayRoundedIcon sx={{ml: 1}} htmlColor='#818283' /> 
+            <InputBase
+              id='date-input'
+              placeholder='Date (yyyy-mm-dd)'
+              type='text'
+              //value={attendance.date}
+              //onKeyDown={handleKeyPress}
+              inputProps={{
+                  "aria-readonly": 'true'
+                  }}
+              sx={{ flex: 1, marginLeft: '10px', fontSize: '18px' }}/>
+          </Paper>          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
+
+{/* <div className="page">
     <h2>Login Page</h2>
     <div className="inputs">
       <div className='input'>
@@ -47,8 +94,4 @@ const Login = () => {
           <div className='error'>{errorMessage}</div> : null 
         }
     </div>
-  </div>
-  )
-}
-
-export default Login
+  </div> */}
