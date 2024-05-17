dadf3605005c66df772632ae3dd3fda91de0483a
antoalex07 <antoalex710@gmail.com>
2024-05-17 23:44:03 +0530

    login page completed

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,8 +2,9 @@ import React, { useReducer, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthData } from '../../auth/Authwrapper';
 import { KeyboardBackspaceRounded } from '@mui/icons-material';
-import TodayRoundedIcon from '@mui/icons-material/TodayRounded';
-import { InputBase, Paper } from '@mui/material';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+import FingerprintRoundedIcon from '@mui/icons-material/FingerprintRounded';
+import { Button, InputBase, Paper, ThemeProvider, createTheme } from '@mui/material';
 
 
 const Login = () => {
@@ -24,20 +25,66 @@ const Login = () => {
         }
     }
 
+    const goBack = () => {
+      navigate(-1);
+    }
+
+    const theme = createTheme({
+      palette: {
+        primary: {
+          main: '#11636f',
+        },
+      },
+    });
+
+    const handleKeyPress = (event) => {
+      if(event.key === 'Enter'){
+          event.preventDefault();
+      }
+  }
+
   return (
     <div className="login">
       <div className='form-container'>
         <div className='image-container'>
+          
           <KeyboardBackspaceRounded 
                             htmlColor='#818283'
                             style={{ position: 'absolute',
                                     top: '30px',
                                     left: '30px',
                                     fontSize: '40px',   
-                                }}/>
+                                }}
+                            onClick={goBack}/>
+        
         </div>
         <div className='login-form'>
           <h1 className='heading'>Login</h1>
+          
+          <Paper
+            component="form"
+            sx={{ marginTop: '50px',
+                  marginLeft: '12px',
+                  p: '10px 4px',
+                  display: 'flex',
+                  width: 300,
+                  borderRadius: '0px',
+                  alignItems: 'center',
+                  backgroundColor: '#0da5b0' }}>
+            
+            <PersonRoundedIcon sx={{ml: 1}} htmlColor='#fffff' /> 
+            
+            <InputBase
+              id='userName'
+              placeholder='Enter User Name'
+              type='text'
+              onChange={(event) => {
+                setFormData({userName: event.target.value})
+              }}
+              onKeyDown={handleKeyPress}
+              sx={{ flex: 1, marginLeft: '10px', fontSize: '18px' }}/>
+          </Paper>
+          
           <Paper
             component="form"
             sx={{ marginTop: '6%',
@@ -47,19 +94,38 @@ const Login = () => {
                   width: 300,
                   borderRadius: '0px',
                   alignItems: 'center',
-                  backgroundColor: '#c5e6f5' }}>
-            <TodayRoundedIcon sx={{ml: 1}} htmlColor='#818283' /> 
+                  backgroundColor: '#0da5b0' }}>
+            
+            <FingerprintRoundedIcon sx={{ml: 1}} htmlColor='#fffff' /> 
+            
             <InputBase
-              id='date-input'
-              placeholder='Date (yyyy-mm-dd)'
-              type='text'
-              //value={attendance.date}
-              //onKeyDown={handleKeyPress}
-              inputProps={{
-                  "aria-readonly": 'true'
-                  }}
+              id='password'
+              placeholder='Enter Password'
+              type='password'
+              onChange={(event) => {
+                setFormData({password: event.target.value})
+              }}
+              onKeyDown={handleKeyPress}
               sx={{ flex: 1, marginLeft: '10px', fontSize: '18px' }}/>
-          </Paper>          
+          </Paper>
+          
+          <ThemeProvider theme={theme}>
+            <Button variant='contained'  
+                    style={{ fontSize: '20px',
+                              width: 310,
+                              borderRadius: '0px',
+                              marginLeft: '12px',
+                              marginTop: '70px'
+                            }}
+                    onClick={doLogin}
+                    >
+                        Submit</Button>
+          </ThemeProvider>
+          
+          {errorMessage ? <div className='error-message'>{errorMessage}</div> : null}          
+
+          <a href='/signup'>Create a new Account</a>
+
         </div>
       </div>
     </div>
@@ -67,31 +133,3 @@ const Login = () => {
 }
 
 export default Login
-
-{/* <div className="page">
-    <h2>Login Page</h2>
-    <div className="inputs">
-      <div className='input'>
-        <input 
-          //defaultValue={formData.userName} 
-          onChange={(e) => 
-            setFormData({userName: e.target.value})}
-          type="text"
-          />
-        </div>
-        <div className='input'>
-          <input 
-            //defaultValue={formData.password}
-            onChange={(e) => 
-              setFormData({password: e.target.value})}
-            type='password'
-          />
-        </div>
-        <div className='button'>
-          <button onClick={doLogin}>Log in</button>
-        </div>
-        {errorMessage ?
-          <div className='error'>{errorMessage}</div> : null 
-        }
-    </div>
-  </div> */}
